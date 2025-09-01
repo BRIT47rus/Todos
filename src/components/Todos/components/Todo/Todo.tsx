5d4c04143e9b4486aa33b6eaf3f00f4b7116b9d5
Vladimir Noutbook <vovirus2@inbox.ru>
2025-09-01 16:59:12 +0300

    add func delete Todo

--- src/components/Todos/components/Todo/Todo.tsx
+++ src/components/Todos/components/Todo/Todo.tsx
@@ -5,16 +5,25 @@ import { Input } from '../../../features/components/Input/Input';
 import './Todo.css';
 import type { ITodo } from '../../../App/types';
 import cls from 'classnames';
+import { useTodosCTX } from '../../../App/hooks';
 
 export const Todo: FC<ITodo> = ({ title, checked, id }) => {
+    const { todos, setTodos } = useTodosCTX();
+
+    console.log(todos);
+    const onclickDeleteTodo = (id: number) => {
+        if (id !== undefined) {
+            setTodos((prev) => prev.filter((todo) => todo.id !== id));
+        }
+    };
     return (
         <div className="todo">
             <Input checkedProps={checked} todoId={id} />
-            <span className={cls({ 'todo__text-compleate': checked })}>
+            <span className={cls({ 'todo__text-compleate': !checked })}>
                 {title}
             </span>
 
-            <div className="todo__button">
+            <div className="todo__button" onClick={() => onclickDeleteTodo(id)}>
                 <Button element={<IconDelete />} />
             </div>
         </div>
