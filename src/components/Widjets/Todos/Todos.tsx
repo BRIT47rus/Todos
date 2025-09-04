e87d39aaab7639a166fc588976e0e3790ffa6e4f
Vladimir Noutbook <vovirus2@inbox.ru>
2025-09-04 17:46:37 +0300

    complete animation todo

--- src/components/Widjets/Todos/Todos.tsx
+++ src/components/Widjets/Todos/Todos.tsx
@@ -3,57 +3,38 @@ import { Title } from '../../features/components/Title/Title';
 import { Input } from '../../features/components/Input/Input';
 import './Todos.css';
 import { Info, Todo } from '../../features';
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
-import React, { useRef } from 'react';
+
+export type TStyleAnimation = 'in' | 'out' | 'enter';
 
 export const Todos = () => {
     const { filteredTodos, addTodo } = useTodosCTX();
 
-    const nodeRefs = React.useRef<{
-        [key: string]: React.RefObject<HTMLDivElement>;
-    }>({});
-    filteredTodos.forEach(({ id }) => {
-        if (!nodeRefs.current[id]) {
-            //@ts-ignore
-            nodeRefs.current[id] = React.createRef<HTMLDivElement>(); // createRef для более строгого типа
-        }
-    });
     return (
         <div className={'todos'}>
             <div className={'todos__input-text'}>
                 <Input type="text" onAdd={addTodo} />
             </div>
-            <TransitionGroup component="div" className="todos__list">
-                {filteredTodos.length ? (
-                    filteredTodos.map(({ title, checked, id }) => {
-                        return (
-                            <CSSTransition
-                                key={id}
-                                nodeRef={nodeRefs.current[id]}
-                                timeout={300}
-                                classNames="todo"
-                                onExited={() => {
-                                    console.log('');
-                                }}
-                            >
-                                <Todo
-                                    ref={nodeRefs.current[id]}
-                                    title={title}
-                                    checked={checked}
-                                    id={id}
-                                />
-                            </CSSTransition>
-                        );
-                    })
-                ) : (
-                    <div className="todos__empty">
-                        <Title Element={'h2'} classNames="title-h2">
-                            Cписок пуст
-                        </Title>
-                    </div>
-                )}
-                <Info />
-            </TransitionGroup>
+            {filteredTodos.length ? (
+                filteredTodos.map(({ id, title, checked }) => {
+                    return (
+                        <div
+                            key={id}
+                            // className={
+                            //     // animation && `todo-animation-${animation}`
+                            // }
+                        >
+                            <Todo title={title} checked={checked} id={id} />
+                        </div>
+                    );
+                })
+            ) : (
+                <div className="todos__empty">
+                    <Title Element={'h2'} classNames="title-h2">
+                        Cписок пуст
+                    </Title>
+                </div>
+            )}
+            <Info />
         </div>
     );
 };
--- src/components/features/Todo/Todo.tsx
+++ src/components/features/Todo/Todo.tsx
@@ -1,5 +1,4 @@
 import cls from 'classnames';
-import { forwardRef } from 'react';
 import { formatText } from '../../App/helpers';
 import { useTodosCTX } from '../../App/hooks';
 import type { ITodo } from '../../App/types';
@@ -7,25 +6,52 @@ import { Button } from '../components/Button/Button';
 import { IconDelete } from '../components/IconDelete/IconDelete';
 import { Input } from '../components/Input/Input';
 import './Todo.css';
+import { useEffect, useRef, useState, type FC } from 'react';
+import type { TStyleAnimation } from '../../Widjets/Todos/Todos';
 
-export const Todo = forwardRef<HTMLDivElement, ITodo>(
-    ({ title, checked, id }, ref) => {
-        const { deleteTodo } = useTodosCTX();
+export const Todo: FC<ITodo> = ({ title, checked, id }) => {
+    const { deleteTodo } = useTodosCTX();
+    const ref = useRef<HTMLDivElement>(null);
+    const [animation, setAnimation] = useState<TStyleAnimation>();
 
-        return (
-            <div className={'todo'} ref={ref}>
-                <Input checkedProps={checked} todoId={id} onAdd={() => null} />
-                <span className={cls({ 'todo__text-compleate': !checked })}>
-                    {formatText(title)}
-                </span>
+    useEffect(() => {
+        setAnimation('in');
+        const enterTime = setTimeout(() => {
+            setAnimation('enter');
+        }, 300);
 
-                <div className="todo__button">
-                    <Button
-                        element={<IconDelete />}
-                        onClick={() => deleteTodo(id)}
-                    />
-                </div>
+        return () => {
+            clearTimeout(enterTime);
+        };
+    }, []);
+    const onAnimationEnd = (id: number) => {
+        if (animation === 'out') {
+            deleteTodo(id);
+        }
+    };
+    const handleDelete = () => {
+        setAnimation('out');
+    };
+
+    return (
+        <div
+            ref={ref}
+            className={cls('todo', animation && `todo-animation-${animation}`)}
+            onAnimationEnd={() => onAnimationEnd(id)}
+        >
+            <Input checkedProps={checked} todoId={id} onAdd={() => null} />
+            <span className={cls({ 'todo__text-compleate': !checked })}>
+                {formatText(title)}
+            </span>
+
+            <div className="todo__button">
+                <Button
+                    element={<IconDelete />}
+                    onClick={() => {
+                        handleDelete();
+                    }}
+                />
             </div>
-        );
-    }
-);
+        </div>
+    );
+};
