93ab6df12292d01657922a4f0d94571041e4b731
Vladimir Noutbook <vovirus2@inbox.ru>
2025-09-04 14:09:55 +0300

    save midle

--- src/components/features/components/Button/Button.tsx
+++ src/components/features/components/Button/Button.tsx
@@ -1,12 +1,20 @@
-import type { FC, HTMLAttributes, ReactNode } from 'react';
+import { type FC, type HTMLAttributes, type ReactNode } from 'react';
 import './Button.css';
+import classNames from 'classnames';
+import type { ActionFilter } from '../../../App/types';
 interface Props extends HTMLAttributes<HTMLButtonElement> {
     element: ReactNode;
+    active?: ActionFilter;
     onClick: () => void;
 }
-export const Button: FC<Props> = ({ element, onClick, ...rest }) => {
+export const Button: FC<Props> = ({ element, onClick, active, ...rest }) => {
     return (
-        <div className="button" onClick={onClick}>
+        <div
+            className={classNames('button', {
+                'info-filter__button-active': active === element,
+            })}
+            onClick={onClick}
+        >
             <button {...rest}></button>
             {element}
         </div>
--- src/components/features/components/InfoFilter/InfoFilter.tsx
+++ src/components/features/components/InfoFilter/InfoFilter.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useTodosCTX } from '../../../App/hooks';
 import type { ActionFilter } from '../../../App/types';
 import { Button } from '../Button/Button';
@@ -5,14 +6,22 @@ import './InfoFilter.css';
 export const InfoFilter = () => {
     const { filterTodos } = useTodosCTX();
     const actions: ActionFilter[] = ['all', 'completed', 'active'];
+    const [selected, setSelected] = useState<ActionFilter>('all');
+
+    const onClickBTNFilter = (item: ActionFilter) => {
+        setSelected(item);
+        filterTodos(item);
+    };
+
     return (
         <div className="info-filter">
             {actions.map((item, i) => {
                 return (
                     <Button
                         key={i}
-                        onClick={() => filterTodos(item)}
+                        onClick={() => onClickBTNFilter(item)}
                         element={item}
+                        active={selected}
                     />
                 );
             })}
